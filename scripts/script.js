8e7634b3606b084983660efb55e473d5e36f3d07
ZThompson1 <zacharygthompson@gmail.com>
2024-11-20 08:32:52 -0500

    Create script.js
    I guess?

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+/**
+ * Close your eyes, 
+ * best possible 
+ * timelines:
+ * 
+ * "Kiss into infinity
+ *  with or without me
+ *  with or without
+ *  we'll wind up living
+ *  with our talents"
+ */
